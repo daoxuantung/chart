4cde8c8b4c6c60bde1226726f1d48ff34a2dd4eb
mume-buihanh <92332723+mume-buihanh@users.noreply.github.com>
2021-11-29 15:04:43 +0700

    Change date time

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import { useRef, useLayoutEffect } from "react";
-import "./App.css";
+import {
+  useRef,
+  useLayoutEffect,
+  useState,
+  forwardRef,
+  LegacyRef,
+} from "react";
+import DatePicker from "react-datepicker";
+import { getYear } from "date-fns";
+import range from "lodash/range";
+
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import { XYChart } from "@amcharts/amcharts4/charts";
 
+import "react-datepicker/dist/react-datepicker.css";
+import "./App.css";
+
 am4core.useTheme(am4themes_animated);
 am4core.addLicense("ch-custom-attribution");
 
 function App() {
+  const [startDate, setStartDate] = useState<Date>(new Date());
+  const [endDate, setEndDate] = useState<Date>(new Date());
+  const [date, setDate] = useState<{ startDate?: Date; endDate?: Date }>({
+    startDate: undefined,
+    endDate: undefined,
+  });
   const chartRef = useRef<XYChart>();
+  const years = range(1990, getYear(new Date()) + 1, 1);
 
   useLayoutEffect(() => {
+    chartRef.current = (<div>Loading</div>) as any;
     (async () => {
       let chart = am4core.create("chart", am4charts.XYChart);
       chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
-
       chart.paddingRight = 30;
       chart.dateFormatter.dateFormat = "yyyy-MM-dd";
       chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
@@ -24,9 +43,7 @@ function App() {
       colorSet.saturation = 0.4;
 
       const res = await fetch("https://my-db-chart.herokuapp.com/charts");
-
       const data = await res.json();
-
       chart.data = data;
 
       let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
@@ -37,7 +54,8 @@ function App() {
       let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
       dateAxis.renderer.minGridDistance = 70;
       dateAxis.baseInterval = { count: 1, timeUnit: "day" };
-      // dateAxis.max = new Date(2018, 0, 1, 24, 0, 0, 0).getTime();
+      dateAxis.max = date.endDate?.getTime();
+      dateAxis.min = date.startDate?.getTime();
       dateAxis.strictMinMax = true;
       dateAxis.renderer.tooltipLocation = 0;
 
@@ -45,7 +63,6 @@ function App() {
       series1.columns.template.height = am4core.percent(70);
       series1.columns.template.tooltipText =
         "{task}: [bold]{openDateX}[/] - [bold]{dateX}[/]";
-
       series1.dataFields.openDateX = "start";
       series1.dataFields.dateX = "end";
       series1.dataFields.categoryY = "category";
@@ -62,8 +79,168 @@ function App() {
         chart.dispose();
       };
     })();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [date]);
 
-  return <div id="chart" style={{ width: "100%", height: "500px" }}></div>;
+  const ChangeStartDateInput = forwardRef(
+    ({ value, onClick }: { value?: Date; onClick?: () => void }, ref) => (
+      <button
+        className="custom-input"
+        onClick={onClick}
+        ref={ref as unknown as LegacyRef<HTMLButtonElement>}
+      >
+        {value}
+      </button>
+    )
+  );
+
+  const ChangeEndDateInput = forwardRef(
+    ({ value, onClick }: { value?: Date; onClick?: () => void }, ref: any) => (
+      <button
+        className="custom-input"
+        onClick={onClick}
+        ref={ref as unknown as LegacyRef<HTMLButtonElement>}
+      >
+        {value}
+      </button>
+    )
+  );
+
+  return (
+    <div>
+      <div id="chart" className="chart"></div>
+      <div className="datepicker-box">
+        <div className="datepicker-box__title">From:</div>
+        <DatePicker
+          customInput={<ChangeStartDateInput />}
+          calendarClassName="datepicker-box__calendar"
+          selectsStart
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+          renderCustomHeader={({
+            date,
+            changeYear,
+            decreaseYear,
+            increaseYear,
+            prevYearButtonDisabled,
+            nextYearButtonDisabled,
+          }) => (
+            <div
+              style={{
+                margin: 10,
+                display: "flex",
+                justifyContent: "space-between",
+                paddingLeft: 18,
+                paddingRight: 18,
+              }}
+            >
+              <button
+                onClick={decreaseYear}
+                disabled={prevYearButtonDisabled}
+                style={{ color: "#fff" }}
+              >
+                {"<"}
+              </button>
+              <select
+                style={{ width: "60px", outline: "none", cursor: "pointer" }}
+                value={getYear(date)}
+                onChange={({ target: { value } }) =>
+                  changeYear(parseInt(value))
+                }
+              >
+                {years.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+
+              <button
+                onClick={() => {
+                  if (getYear(date) < 2021) increaseYear();
+                }}
+                disabled={getYear(date) === 2021}
+                style={{ color: "#fff" }}
+              >
+                {">"}
+              </button>
+            </div>
+          )}
+          selected={startDate}
+          onChange={(date) => setStartDate(date as Date)}
+        />
+        <div className="datepicker-box__title">To:</div>
+        <DatePicker
+          customInput={<ChangeEndDateInput />}
+          calendarClassName="datepicker-box__calendar"
+          selectsEnd
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+          renderCustomHeader={({
+            date,
+            changeYear,
+            decreaseYear,
+            increaseYear,
+            prevYearButtonDisabled,
+            nextYearButtonDisabled,
+          }) => (
+            <div
+              style={{
+                margin: 10,
+                display: "flex",
+                justifyContent: "space-between",
+                paddingLeft: 18,
+                paddingRight: 18,
+              }}
+            >
+              <button
+                onClick={() => decreaseYear}
+                disabled={prevYearButtonDisabled}
+                style={{ color: "#fff" }}
+              >
+                {"<"}
+              </button>
+              <select
+                style={{ width: "60px", outline: "none", cursor: "pointer" }}
+                value={getYear(date)}
+                onChange={({ target: { value } }) =>
+                  changeYear(parseInt(value))
+                }
+              >
+                {years.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+              <button
+                onClick={() => {
+                  if (getYear(date) < 2021) increaseYear();
+                }}
+                disabled={getYear(date) === 2021}
+                style={{ color: "#fff" }}
+              >
+                {">"}
+              </button>
+            </div>
+          )}
+          selected={endDate}
+          onChange={(date) => setEndDate(date as Date)}
+        />
+        <button
+          className="custom-input custom-input--submit"
+          onClick={() => setDate({ endDate, startDate })}
+        >
+          Apply
+        </button>
+        <button
+          className="custom-input custom-input--submit"
+          onClick={() => setDate({})}
+        >
+          Clear
+        </button>
+      </div>
+    </div>
+  );
 }
 export default App;
